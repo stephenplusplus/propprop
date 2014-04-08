cbfbbffe798eed6fddf1924bedf0fe1161ba143d
Stephen Sawchuk <sawchuk@gmail.com>
2014-04-08 15:42:57 -0400

    forget that error stuff.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function (propertyName) {
     if (propertyName in item) {
       return item[propertyName];
     } else {
-      throw new Error('"' + propertyName + '" not found. ' + JSON.stringify(item, null, 2));
+      return item;
     }
   };
 };
