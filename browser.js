db843d4d94b33b546ce0fefc93f87467de7bebc2
Stephen Sawchuk <sawchuk@gmail.com>
2014-04-14 14:47:24 -0400

    man.

--- browser.js
+++ browser.js
@@ -3,8 +3,6 @@ module.exports = function (propertyName) {
   return function (item) {
     if (propertyName in item) {
       return item[propertyName];
-    } else {
-      return item;
     }
   };
 };
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ module.exports = function (propertyName) {
   return function (item) {
     if (propertyName in item) {
       return item[propertyName];
-    } else {
-      return item;
     }
   };
 };
